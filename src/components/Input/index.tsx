c83a96f41c849f6eda47356e4d1c80ffaf2be280
Lucas Prado <lucasntc2201@gmail.com>
2022-03-15 17:14:55 -0300

    Added typescript in the project and refactored all components

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -9,8 +9,14 @@ import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface InputProps {
+  name: string;
+  Icon?: any;
+  placeholder: string;
+}
+
+export function Input ({ name, Icon, ...rest }: InputProps) {
+  const inputRef = useRef<any>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
@@ -49,5 +55,3 @@ const Input = ({ name, icon: Icon, ...rest }) => {
     </Container>
   );
 };
-
-export default Input;
